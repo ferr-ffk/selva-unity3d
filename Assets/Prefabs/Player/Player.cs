73a05cf86a37ada1dfa954b1db283a316cc41480
Fernando Freitas <freitax.li@gmail.com>
2025-04-18 15:49:16 -0300

    feat(player): added camera rotation and player movement

--- Assets/Prefabs/Player/Player.cs
+++ Assets/Prefabs/Player/Player.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,19 +10,38 @@ public class Player : MonoBehaviour
     [SerializeField]
     private InputActionReference _movementReference;
 
+    [SerializeField]
+    private InputActionReference _lookReference;
+
+    [SerializeField]
+    private GameObject _camera;
+
+    [SerializeField]
+    private GameObject _armature;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 movementInput = _movementReference.action.ReadValue<Vector2>();
+        // Obtém o vetor 2D de direção a partir do input
+        Vector2 joystickDirection = _movementReference.action.ReadValue<Vector2>();
 
-        Vector3 movementDirection = new(movementInput.x * Time.deltaTime, 0, movementInput.y * Time.deltaTime);
+        // Cria um vetor 3D para movimentar apenas no x e z (sendo Y a vertical)
+        Vector3 direction = new(joystickDirection.x, 0, joystickDirection.y);
 
-        _movementComponent.Move(gameObject, movementDirection);
+        // Utiliza a rotação que a câmera está apontando, e usa para que o jogador se movimente de acordo com ela
+        Quaternion cameraRotation = Quaternion.Euler(0, _camera.transform.eulerAngles.y, 0);
+
+        Quaternion desiredRotation = Quaternion.LookRotation(direction);
+
+        // Rotaciona a armação do jogador para a direção desejada
+        //_armature.transform.rotation = Quaternion.Slerp(_armature.transform.rotation, desiredRotation, Time.deltaTime * 8);
+
+        // Move o Jogador
+        _movementComponent.Move(gameObject, direction, cameraRotation);
     }
 }
