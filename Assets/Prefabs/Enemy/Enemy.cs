897f6e8c70c558e01551e0460115cf3b3eef952c
Fernando Freitas <freitax.li@gmail.com>
2025-05-17 12:16:40 -0300

    feat(enemy): added basic implementation of damage and health logic

--- Assets/Prefabs/Enemy/Enemy.cs
+++ Assets/Prefabs/Enemy/Enemy.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private TextMesh _healthPorcentage;
+
+    [SerializeField]
+    private HealthComponent _healthComponent;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _healthPorcentage.text = (_healthComponent.GetHealthPorcentage() * 100).ToString() + "%";
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnHealthChanged(float health)
+    {
+        _healthPorcentage.text = (_healthComponent.GetHealthPorcentage() * 100).ToString() + "%";
     }
 }
