e27a823b86c75f106d65591991121aaf21d9f6c9
Fernando Freitas <freitax.li@gmail.com>
2025-05-19 16:39:49 -0300

    chore(test): created debug scene and script for testing quick time events

---
+++ Assets/Test/QTETest/QuickTimeEventHandler.cs
@@ -0,0 +1,55 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class QuickTimeEventHandler : MonoBehaviour
+{
+    public QuickTimeEventComponent quickTimeEventComponent;
+
+    public TextMeshProUGUI textMesh;
+
+    public TextMeshProUGUI description;
+
+    public InputActionReference eventStart;
+
+    private void Start()
+    {
+        textMesh.text = quickTimeEventComponent.TimeRemaining.ToString();
+
+        description.text = "aperta 'c' p começar";
+    }
+
+    private void Update()
+    {
+        if (eventStart.action.triggered)
+        {
+            quickTimeEventComponent.StartEvent();
+        }
+
+        textMesh.text = quickTimeEventComponent.TimeRemaining.ToString();
+    }
+
+    public void OnEventStart()
+    {
+        Debug.Log("Event started");
+
+        textMesh.text = quickTimeEventComponent.TimeRemaining.ToString();
+
+        description.text = quickTimeEventComponent.GetEventDescription();
+    }
+
+    public void OnEventSuccess()
+    {
+        description.text = "suceso :)";
+
+        Debug.Log("Event succeeded");
+    }
+
+    public void OnEventFailure()
+    {
+        description.text = "faliou :(";
+
+        Debug.Log("Event failed");
+    }
+}
